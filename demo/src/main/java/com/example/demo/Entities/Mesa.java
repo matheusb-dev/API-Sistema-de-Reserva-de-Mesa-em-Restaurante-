3eda59d4cf79232a06a2f84be32381eda4965203
Gabriel-S-Mendes <mesente12@gmail.com>
2025-05-19 22:05:39 -0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Entities/Mesa.java
@@ -0,0 +1,29 @@
+package com.example.demo.Entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+@Data
+@Entity
+@Table(name = "Mesa")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer numero;
+
+     @Column(nullable = false)
+    private Integer capacidade;
+
+    @Column(nullable = false)
+    private String status;
+
+}
